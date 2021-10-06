fc38ff5f10ae43babb86d5126741e4afdccabcd7
YuliyaKozhulenko <89405746+YuliyaKozhulenko@users.noreply.github.com>
2021-10-06 20:03:28 +0200

    Add files via upload

---
+++ src/stepit/hw/ArrayRemove.java
@@ -0,0 +1,13 @@
+package stepit.hw;
+
+public class ArrayRemove {
+    public static void main(String[] args) {
+        int[] numArray = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        for (int i = 0; i < numArray.length; i++) {
+            double a = numArray[i] * 0.25;
+            double b = numArray[i] - a;
+            System.out.println(b);
+        }
+    }
+}
+
